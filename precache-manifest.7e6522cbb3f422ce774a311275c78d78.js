699626f084afb3ea09cd23179b46bb230b22eaf1
Mihai Filip <voidthevillain@users.noreply.github.com>
2020-03-19 18:37:06 +0100

    Updates

---
+++ precache-manifest.7e6522cbb3f422ce774a311275c78d78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7802506af90913abfb88b17ea9a319",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "327b4136e5f336527378",
+    "url": "/monsters-rolodex/static/css/main.eaf18edd.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "327b4136e5f336527378",
+    "url": "/monsters-rolodex/static/js/main.77ef481d.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
